dde84bbdb19455cae7777bfc2b7666a602437de1
Ryan Hanson <ryan@codesploit.com>
2017-11-13 22:36:54 -0800

    Initial source commit

---
+++ ExternalC2Web/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ExternalC2Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            // TODO: Add more elegant command line parsing
+            return WebHost.CreateDefaultBuilder(args)
+                .UseUrls(args[1])
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
